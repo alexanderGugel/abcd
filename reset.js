fba22d1ea4691a445adf872460b80809b8d4767d
Alexander Gugel <alexander.gugel@gmail.com>
2014-10-02 01:27:07 +0200

    Implement register() and login()

--- reset.js
+++ reset.js
@@ -3,37 +3,36 @@ var query = require('./query');
 query(
   'DROP TABLE IF EXISTS "user" CASCADE;' +
 
+  'CREATE EXTENSION IF NOT EXISTS "uuid-ossp";' +
+
   'CREATE TABLE IF NOT EXISTS "user" (' +
-    'id BIGSERIAL PRIMARY KEY NOT NULL,' +
+    'id BIGSERIAL PRIMARY KEY,' +
     'email TEXT NOT NULL UNIQUE,' +
     'password TEXT NOT NULL' +
   ');' +
 
+  'CREATE TABLE IF NOT EXISTS "token" (' +
+    'id BIGSERIAL PRIMARY KEY,' +
+    'token UUID NOT NULL UNIQUE DEFAULT uuid_generate_v4(),' +
+    'user_id BIGSERIAL NOT NULL REFERENCES "user"(id)' +
+  ');' +
+
   'CREATE TABLE IF NOT EXISTS "experiment" (' +
-    'id BIGSERIAL PRIMARY KEY NOT NULL,' +
+    'id BIGSERIAL PRIMARY KEY,' +
     'user_id BIGSERIAL NOT NULL REFERENCES "user"(id),' +
     'name TEXT NOT NULL,' +
     'running BOOLEAN NOT NULL' +
   ');' +
 
   'CREATE TABLE IF NOT EXISTS "variant" (' +
-    'id BIGSERIAL PRIMARY KEY NOT NULL,' +
+    'id BIGSERIAL PRIMARY KEY,' +
     'name TEXT NOT NULL,' +
     'control BOOLEAN NOT NULL,' +
     'experiment_id BIGSERIAL NOT NULL REFERENCES "experiment"(id)' +
+  ');' +
+
+  'CREATE TABLE IF NOT EXISTS "request" (' +
+    'id BIGSERIAL PRIMARY KEY,' + // TODO: More columns
+    'variant_id BIGSERIAL NOT NULL REFERENCES "variant"(id)' +
   ');'
 );
-
-//
-// query(
-//   'CREATE TABLE IF NOT EXISTS variant (' +
-//     'variantid INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,' +
-//     'variantname TEXT NOT NULL,' +
-//     'variantcontrol BOOLEAN NOT NULL CHECK (variantcontrol IN (0,1)),' +
-//     'variantexperiment INTEGER NOT NULL,' +
-//     'FOREIGN KEY(variantexperiment) REFERENCES experiment(experimentid)' +
-//   ')',
-//   function () {
-//     console.log(arguments);
-//   }
-// );
--- user.js
+++ user.js
@@ -1,17 +1,46 @@
-var db = require('./db');
+var query = require('./query');
 var bcrypt = require('bcrypt-nodejs');
 
 var register = function (email, password, callback) {
-  db.prepare('SELECT COUNT(1) FROM user WHERE useremail = ?').run(email).finalize(function () {
-    console.log(arguments);
+  email = email.toLowerCase();
+  bcrypt.hash(password, null, null, function (err, password) {
+    query('INSERT INTO "user" (email, password) VALUES ($1, $2)', [email, password], function (error) {
+      if (!error) {
+        return callback();
+      }
+      if (error.code === 23505) {
+        return callback(new Error('User account already exists'));
+      }
+      return callback(new Error('Something went wrong'));
+    });
   });
 };
 
 var login = function (email, password, callback) {
+  email = email.toLowerCase();
+  query('SELECT id, password FROM "user" WHERE email = $1', [email], function (err, result) {
+    rows = result.rows;
+    if (rows.length === 0) {
+      return callback(new Error('Account does not exist'));
+    }
+    bcrypt.compare(password, rows[0].password, function (err, correct) {
+      if (!correct) {
+        return callback(new Error('Invalid password'));
+      }
+      query('INSERT INTO "token" (user_id) VALUES ($1) RETURNING token', [rows[0].id], function (err, result) {
+        callback(null, result.rows[0].token);
+      });
+    });
+  });
 };
 
 var changePassword = function (email, oldPassword, newPassword) {
 
 };
 
-register('alexander.gugel@gmail.com', 'test');
+// register('alexander.gugel@gmail.com', 'test', function () {
+//   console.log(arguments);
+// });
+login('alexander.gugel@gmail.com', 'test', function () {
+  console.log(arguments);
+});

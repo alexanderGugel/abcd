030d09aeefb267b160f070096141b3e23ccac5e4
Alexander Gugel <alexander.gugel@gmail.com>
2014-10-12 18:51:25 +0200

    Get rid of all the old stuff

--- static/js/docs.js
+++
@@ -1,6 +0,0 @@
-module.exports = function (context) {
-  $(function () {
-    $('section').hide();
-    $('#docs').show();
-  });
-};
--- static/js/main.js
+++
@@ -1,9 +0,0 @@
-// if (localStorage.getItem('newbie') && !localStorage.getItem('hide-getting-started')) {
-//   $('#getting-started').show();
-//   $('#getting-started').on('click', '.close', function () {
-//     localStorage.setItem('hide-getting-started', new Date().getTime());
-//     $('#getting-started').hide();
-//   })
-// }
-
-require('./router');
